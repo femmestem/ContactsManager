fb937a15bc1b2a43dae4b53307f83e6d45f772ce
Christine L. Feaster <feasterc@gmail.com>
2015-07-03 22:00:12 -0700

    Refactor Contact model tests to use factory object

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,104 +5,40 @@ describe Contact do
     expect(FactoryGirl.build(:contact)).to be_valid
   end
 
-  it "is valid with a firstname, lastname, and email" do
-    contact = Contact.new(
-      firstname: "Christine",
-      lastname: "Feaster",
-      email: "cfeaster@test.com"
-      )
-
-    expect(contact).to be_valid
-  end
-
   it "is invalid without a firstname" do
-    contact = Contact.new(
-      firstname: nil
-      )
-
+    contact = Contact.new(firstname: nil)
     expect(contact).to have(1).errors_on(:firstname)
   end
 
   it "is invalid without a lastname" do
-      contact = Contact.new(
-      lastname: nil
-      )
-
+    contact = Contact.new(lastname: nil)
     expect(contact).to have(1).errors_on(:lastname)
   end
 
   it "is invalid without an email address" do
-    contact = Contact.new(
-      email: nil
-      )
-
+    contact = Contact.new(email: nil)
     expect(contact).to have(1).errors_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-    john = Contact.create(
-      firstname: "John",
-      lastname: "Doe",
-      email: "jdoe@test.com"
-      )
-    jane = Contact.new(
-      firstname: "Jane",
-      lastname: "Doe",
-      email: "jdoe@test.com"
-      )
+    FactoryGirl.create(:contact, email: "jdoe@test.com")
+    contact = FactoryGirl.build(:contact, email: "jdoe@test.com")
 
-    expect(jane).to have(1).errors_on(:email)
+    expect(contact).to have(1).errors_on(:email)
   end
 
   it "returns a contact's full name as string" do
-    contact = Contact.new(
-      firstname: "Jane",
-      lastname: "Doe",
-      email: "jdoe@test.com"
-      )
-
+    contact = FactoryGirl.build(:contact)
     expect(contact.name).to eq "Jane Doe"
   end
 
   # == Class methods and Scopes
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: "John",
-      lastname: "Smith",
-      email: "jsmith@test.com"
-      )
-    feaster = Contact.create(
-      firstname: "Christine",
-      lastname: "Feaster",
-      email: "cfeaster@test.com"
-      )
-    fennimore = Contact.create(
-      firstname: "Neal",
-      lastname: "Fennimore",
-      email: "nfennimore@test.com"
-      )
-
-    expect(Contact.by_letter("F")).to eq [feaster, fennimore]
-  end
-
   describe "filter last name by letter" do
     before :each do
-      @smith = Contact.create(
-        firstname: "John",
-        lastname: "Smith",
-        email: "jsmith@test.com"
-        )
-      @feaster = Contact.create(
-        firstname: "Christine",
-        lastname: "Feaster",
-        email: "cfeaster@test.com"
-        )
-      @fennimore = Contact.create(
-        firstname: "Neal",
-        lastname: "Fennimore",
-        email: "nfennimore@test.com"
-        )
+      @smith = FactoryGirl.create(:contact, lastname: "Smith")
+      @feaster = FactoryGirl.create(:contact, lastname: "Feaster")
+      @fennimore = FactoryGirl.create(:contact, lastname: "Fennimore")
     end
 
     context "matching letters" do
