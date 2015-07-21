93bd5f90cc82aac9dd2f58907f75315025aaf5b6
Christine L. Feaster <feasterc@gmail.com>
2015-07-20 22:22:28 -0700

    Add tests for 'GET contacts#show'

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -14,8 +14,17 @@ describe ContactsController do
   end
 
   describe 'GET #show' do
-    it "assigns the requested contact to @contact"
-    it "renders the :show template"
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+
+    it "renders the :show template" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(response).to render_template :show
+    end
   end
 
   describe 'GET #new' do
